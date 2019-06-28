5d193b86aabd71907dddaa0adb6256cfd2dc52a1
alexey_eng <alexey_eng@mail.ru>
2019-06-28 15:39:13 +0500

    Overload some UObject operations in UObject

--- Source/ProjCoreObj/Testee/MyObject.cpp
+++ Source/ProjCoreObj/Testee/MyObject.cpp
@@ -1,3 +1,34 @@
 #include "MyObject.h"
 
 #include "Util/Core/LogUtilLib.h"
+
+
+void UMyObject::RegisterDependencies()
+{
+	M_LOGFUNC_MSG(TEXT("%s"), *ULogUtilLib::GetNameAndClassSafe(this));
+	Super::RegisterDependencies();
+}
+
+//void UMyObject::Register(const TCHAR* PackageName, const TCHAR* Name)
+//{
+//	M_LOGFUNC_MSG(TEXT("%s"), *ULogUtilLib::GetNameAndClassSafe(this));
+//	Super::Register(PackageName, Name);
+//}
+
+void UMyObject::DeferredRegister(UClass* UClassStaticClass, const TCHAR* PackageName, const TCHAR* Name)
+{
+	M_LOGFUNC_MSG(TEXT("%s"), *ULogUtilLib::GetNameAndClassSafe(this));
+	Super::DeferredRegister(UClassStaticClass, PackageName, Name);
+}
+
+void UMyObject::BeginDestroy()
+{
+	M_LOGFUNC_MSG(TEXT("%s"), *ULogUtilLib::GetNameAndClassSafe(this));
+	Super::BeginDestroy();
+}
+
+void UMyObject::FinishDestroy()
+{
+	M_LOGFUNC_MSG(TEXT("%s"), *ULogUtilLib::GetNameAndClassSafe(this));
+	Super::FinishDestroy();
+}
--- Source/ProjCoreObj/Testee/MyObject.h
+++ Source/ProjCoreObj/Testee/MyObject.h
@@ -7,5 +7,15 @@ class UMyObject : public UObject
 {
 	GENERATED_BODY()
 
-public:
+protected:
+	// ~ UObjectBase Begin
+	virtual void RegisterDependencies() override;
+	//virtual void Register(const TCHAR* PackageName, const TCHAR* Name) override;
+	virtual void DeferredRegister(UClass* UClassStaticClass, const TCHAR* PackageName, const TCHAR* Name) override;
+	// ~ UObjectBase End
+
+	// ~UObject Begin
+	virtual void BeginDestroy() override;
+	virtual void FinishDestroy() override;
+	// ~UObject End
 };
